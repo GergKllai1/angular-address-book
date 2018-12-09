15a21a65ab21b42420b96b23e6f6ec51f7d49113
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-09 18:49:00 +0100

    Adds tests in app e2e spec and methods for the testing, modifies app component

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,4 +1,5 @@
 import { browser, by, element } from 'protractor';
+import { readElementValue } from '@angular/core/src/render3/util';
 
 export class AppPage {
   navigateTo() {
@@ -8,4 +9,17 @@ export class AppPage {
   getTitleText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getField(name) {
+    return element(by.name(name));
+  }
+
+  getSubmitButton() {
+    return element(by.buttonText('Submit'));
+  }
+
+  cardCount() {
+    return element.all(by.css('.card')).count();
+  }
+
 }
